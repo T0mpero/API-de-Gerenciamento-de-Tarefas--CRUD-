e3d38aa59020069bcbd31d0194e485bdee31adcd
T0mpero <andersoares5000@gmail.com>
2024-09-23 01:25:15 -0300

    criado rota para editar tarefa

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # API de Gerenciamento de Tarefas (CRUD)
- Este é um sistema simples de gerenciamento de tarefas, onde os usuários podem criar, ler, atualizar e deletar tarefas. Ele também possui um sistema de autenticação com JWT, permitindo que os usuários gerenciem suas próprias tarefas.
-
+ Este é um sistema simples de gerenciamento de tarefas, onde os usuários podem criar, ler, atualizar e deletar tarefas. 
 
 ## Tecnologias Utilizadas
 
@@ -8,13 +7,11 @@
 - Express.js
 - MongoDB
 - Mongoose
-- JSON Web Token (JWT)
 
 
 ## Funcionalidades
 
 - Criar, editar, deletar e listar tarefas
-- Autenticação de usuários com JWT
 - Filtro e paginação de tarefas
 - Validação de entrada de dados
 
--- routes/routes.js
+++ routes/routes.js
@@ -20,10 +20,21 @@ router.get('/addTasks', (req, res)=>{
     res.render('createTasks');
 });
 
+// Rota para procurar a task q o usuário deseja editar
+router.get('/editTask/:id', async(req, res)=>{
+    try{
+        const taskId = req.params.id;
+        const task = await Task.findById(taskId);
 
-router.get('/editTask', (req, res)=>{
-    res.render('editTask');
-});
+        if(!task){
+            return res.status(404).send('Tarefa não encontrada');
+        }
+        res.render('editTask', { task }); 
+
+    } catch (err) {
+        console.error('Erro ao buscar tarefa:', err);
+        res.status(500).send('Erro no servidor');
+}});
 
 // Rota para criar uma nova tarefa
 router.post('/createTask', async (req, res) =>{
@@ -37,6 +48,25 @@ router.post('/createTask', async (req, res) =>{
     try{await newTask.save(); res.redirect('/')}
     catch(err) {console.error('Erro ao salvar task', err)};
     
+});
+
+// Rota para editar uma tarefa
+router.post('/editedTask/:id', async (req, res) =>{
+    const updatedData = {
+        title: req.body.newTitle,
+        description : req.body.newDesc
+    }
+    try{
+        const result = await Task.updateOne({_id :req.params.id}, updatedData);
+        if (result.nModified === 0) {
+            return res.status(404).send('Tarefa não encontrada ou não foi alterada.');
+        }
+        res.redirect('/');
+    } catch (err){
+        console.error('Erro ao editar tarefa:', err);
+        res.status(500).send('Erro no servidor');
+    }
+
 });
 
 // Rota para excluir uma tarefa
